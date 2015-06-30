8b0b880d12aa13e82f03bbe78c39caad2b2278b5
Ryan Tracey <ryan.m.tracey@gmail.com>
2015-06-30 13:02:05 -0400

    refactor time

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var easyimg = require('easyimage');
 var async = require('async');
 var format = require('sprintf-js').sprintf;
 var Q = require('q');
+var fs = require('fs');
 
 var path = 'test.jpg';
 var outputTempFilePath = 'temp_%d.png';
 var outputFilePath = 'circle_%d.png';
-var sizes = [150, 100, 125, 33];
+var sizes = [150, 125, 100, 33];
+var size;
 
 (function execute() {
   getDimensions().then(function success(dimensions) {
@@ -35,55 +37,88 @@ function getDimensions() {
 }
 
 function processImages() {
-  var cropAndCircularize = function(size) {
-    return async.series([
-      crop(size),
-      unlink(format(outputTempFilePath, size)),
-      circularize(size),
-      unlink(format(outputFilePath, size))
+  var cropAndCircularize = function(sz) {
+    size = sz;
+    async.series([
+      crop,
+      delay,
+      circularize
     ]);
   };
-  var cb = function(size) {
-    console.log('processed size: ' + size);
-  };
+  // var cb = function(size) {
+  //   console.log('processed size: ' + size);
+  // };
 
-  return async.eachSeries(sizes, function(size, cb) {
-    console.log('processing image size: ' + size);
-    cropAndCircularize(size);
-    cb(size);
-  });
+  //return async.each(sizes, function(size) {
+    console.log('processing image size: ' + sizes[0]);
+    cropAndCircularize(sizes[0]);
+    //cb(size);
+  //});
 }
 
-function crop(size) {
+var delay = function delay(){
+  var defer = Q.defer();
+  setTimeout(function(){
+    console.log("delay complete");
+    defer.resolve("yay");
+  }, 3000);
+  return defer.promise;
+};
+
+var crop = function crop() {
   console.log('cropping: ' + size);
-  return easyimg.exec('convert '+path+' -resize ' +
+  var defer = Q.defer();
+  easyimg.exec('convert '+path+' -resize ' +
     (size) + 'x' + (size) + '^  -gravity center -crop ' +
-    (size + 2) + 'x' + (size + 2) + '+0+0 +repage ' + format(outputTempFilePath, size));
-}
+    (size + 2) + 'x' + (size + 2) + '+0+0 +repage ' + format(outputTempFilePath, size))
+    .then(function(){
+    //   fs.unlink(format(outputTempFilePath, size), function(err){
+    //     if (err) defer.reject(err)
+         defer.resolve('yay');
+    //   });
+    });
+  return defer.promise;
+};
 
-function circularize(size) {
-  console.log('circulizing: ' + size);
+var circularize = function circularize() {
+  console.log('circularizing: ' + size);
   var radius = (size/2) - 1;
   var circleSize = format('%1$d,%1$d %1$d 0', radius);
 
   return easyimg.exec('convert ' + format(outputTempFilePath, size) + ' \\( -size ' + (size) + 'x' + (size) +
   ' xc:none -fill white -draw \'circle ' + circleSize + '\' \\) ' +
   '-compose copy_opacity -composite ' + format(outputFilePath, size));
-}
+};
 
 function unlink(path) {
-  console.log('unlinking: ' + path);
+  // console.log('unlinking: ' + path)
+  // return Q.fcall(fs.unlink(path, function(){console.log('unlinked: ' + path)}));
   var defer = Q.defer();
+  console.log('unlinking: ' + path);
 
-  fs.unlink(path, function (err) {
-    if (err) {
-      console.log("error in unlink");
-      defer.reject(err);
-    } else {
-      console.log("successfully unlinked");
-      defer.resolve();
-    }
-  });
+  fs.unlink(path, function(err) {
+    console.log('unlinked: ' + path);
+
+    if (err) defer.reject(err);
+    defer.resolve("yay!");
+  })
 
   return defer.promise;
+  //console.log('unlinking: ' + path);
+  //return Q.fcall(fs.unlinkSync(path));
+  // var defer = Q.defer();
+  //
+  // console.log('unlinking: ' + path);
+  //
+  // fs.unlink(path, function (err) {
+  //   if (err) {
+  //     console.log("error in unlink");
+  //     defer.reject(err);
+  //   } else {
+  //     console.log("successfully unlinked");
+  //     defer.resolve();
+  //   }
+  // });
+  //
+  // return defer.promise;
 }
