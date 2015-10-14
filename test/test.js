df81bc4f879b6c4b25aa24a40d4e6e08ead52989
Ryan Tracey <ryan.m.tracey@gmail.com>
2015-10-14 11:17:45 -0400

    add some actual tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,13 @@
 var should = require('should');
 
-(5).should.be.exactly(5).and.be.a.Number();
+describe('test one', function(){
+  it('should be equal', function(){
+    (5).should.be.exactly(5).and.be.a.Number();
+  });
+});
+
+describe('test two', function(){
+  it('should be equal', function(){
+    (5).should.be.exactly(5).and.be.a.Number();
+  });
+});
